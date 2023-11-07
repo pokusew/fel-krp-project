24a7fe5e646fa7d5a51d828be9aeefcdb4c46d9d
Martin Endler <pokusew@seznam.cz>
2023-11-07 10:45:45 +0100

    Update WebHID demo to allow basic debugging

--- web/demo.js
+++ web/demo.js
@@ -1,15 +1,63 @@
 "use strict";
 
+// https://developer.mozilla.org/en-US/docs/Web/API/WebUSB_API
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WebHID_API
 // https://wicg.github.io/webhid/
 // https://github.com/WICG/webhid/blob/main/blocklist.txt
 
-const deviceFilter = {
-	vendorId: 0x0483,
-	productId: 0x572b,
-};
-const requestParams = { filters: [deviceFilter] };
-const outputReportId = 0x01;
-const outputReport = new Uint8Array([42]);
+// UI
+
+const dialogElem = document.querySelector('dialog#dialog');
+
+const btnElem = document.querySelector('#demo');
+
+function showMessage(message) {
+	dialogElem.querySelector('#message').textContent = message;
+	dialogElem.showModal();
+}
+
+btnElem.addEventListener('click', (event) => {
+	requestPermissions();
+});
+
+// LOGIC
+
+navigator.hid.addEventListener('connect', handleConnectedDevice);
+navigator.hid.addEventListener('disconnect', handleDisconnectedDevice);
+
+async function requestPermissions() {
+
+	const deviceFilter = {
+		vendorId: 0x1209,
+		productId: 0x0001,
+	};
+
+	const requestParams = { filters: [deviceFilter] };
+
+	try {
+		const devices = await navigator.hid.requestDevice(requestParams);
+		devices.forEach(handleDevice);
+	} catch (err) {
+		document.querySelector('dialog#dialog').showModal();
+		showMessage(err.toString());
+	}
+
+}
+
+async function handleDevice(device) {
+
+	console.log(`handleDevice: ${device.productName} (vendorId=${device.vendorId}, productId=${device.productId})`);
+
+	printCollections(device.collections);
+
+	if (!device.opened) {
+		await device.open();
+	}
+
+	device.addEventListener('inputreport', handleInputReport);
+
+}
 
 function handleConnectedDevice(e) {
 	console.log(`Device connected: ${e.device.productName}`);
@@ -20,20 +68,15 @@ function handleDisconnectedDevice(e) {
 }
 
 function handleInputReport(e) {
-	console.log(`${e.device.productName}: got input report #${e.reportId}`);
+	console.log(`${device.productName} (vendorId=${device.vendorId}, productId=${device.productId}): got input report #${e.reportId}`);
 	console.log(new Uint8Array(e.data.buffer));
 }
 
-navigator.hid.addEventListener('connect', handleConnectedDevice);
-navigator.hid.addEventListener('disconnect', handleDisconnectedDevice);
-
-const btnElem = document.querySelector('#demo');
-
-function printCollections(collections, level = 0) {
+function printCollections(collections, level = 1) {
 
 	// https://developer.mozilla.org/en-US/docs/Web/API/HIDDevice/collections
 
-	const padding = ' '.repeat(level * 2);
+	const padding = '-'.repeat(level * 2);
 
 	for (const collection of collections) {
 		// A HID collection includes usage, usage page, reports, and subcollections.
@@ -61,27 +104,14 @@ function printCollections(collections, level = 0) {
 
 }
 
-btnElem.addEventListener('click', () => {
+navigator.hid.addEventListener('connect', handleConnectedDevice);
+navigator.hid.addEventListener('disconnect', handleDisconnectedDevice);
 
-	navigator.hid.requestDevice(requestParams).then((devices) => {
-		if (devices.length === 0) {
-			return;
-		}
+async function reconnect() {
+	const devices = await navigator.hid.getDevices();
+	devices.forEach(handleDevice);
+}
 
-		const device = devices[0];
-
-		printCollections(device.collections);
-
-		device.open().then(() => {
-			console.log(`Opened device: ${device.productName}`);
-			device.addEventListener('inputreport', handleInputReport);
-			// device.sendReport(outputReportId, outputReport).then(() => {
-			// 	console.log("Sent output report " + outputReportId);
-			// });
-		});
-
-	});
-
-});
+reconnect();
 
 
