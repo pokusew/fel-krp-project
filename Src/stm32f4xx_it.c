7ce3642e843b1564aac8583f169b62c6fce0e009
Martin Endler <pokusew@seznam.cz>
2024-01-09 14:55:05 +0100

    Turn on the Red LED when in HardFault_Handler

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -81,7 +81,7 @@ void NMI_Handler(void) {
   */
 void HardFault_Handler(void) {
 	/* USER CODE BEGIN HardFault_IRQn 0 */
-
+	HAL_GPIO_WritePin(LED3_Red_GPIO_Port, LED3_Red_Pin, GPIO_PIN_SET);
 	/* USER CODE END HardFault_IRQn 0 */
 	while (1) {
 		/* USER CODE BEGIN W1_HardFault_IRQn 0 */
