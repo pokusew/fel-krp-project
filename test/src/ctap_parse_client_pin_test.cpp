6ec3ad1fccba5c1b1f29eef7e250a7e26d171956
Martin Endler <pokusew@seznam.cz>
2025-01-27 05:44:29 +0100

    Unify #include form in ctap_parse_client_pin_test.cpp

--- test/src/ctap_parse_client_pin_test.cpp
+++ test/src/ctap_parse_client_pin_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
-#include "gtest_custom_assertions.h"
+#include <gtest_custom_assertions.h>
 extern "C" {
-#include "ctap_parse.h"
+#include <ctap_parse.h>
 }
 
 namespace {
