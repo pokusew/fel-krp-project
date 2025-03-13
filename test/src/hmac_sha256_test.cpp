b4b7d814df933b4779086f8e3cb28d5d68cdbc83
Martin Endler <pokusew@seznam.cz>
2025-03-13 05:12:33 +0100

    Fix leaks in sha256_test.cpp and hmac_sha256_test.cpp
    The leaks were reported by LeakSanitizer on Ubuntu (in GitHub Actions CI).

--- test/src/hmac_sha256_test.cpp
+++ test/src/hmac_sha256_test.cpp
@@ -2,6 +2,8 @@
 #include <gtest_custom_assertions.h>
 #include <hex.hpp>
 #include <cstring>
+#include <utility>
+#include <vector>
 extern "C" {
 #include <hmac.h>
 }
@@ -10,62 +12,53 @@ namespace {
 constexpr size_t SHA256_INTERNAL_BLOCK_SIZE = 64;
 constexpr size_t SHA256_OUTPUT_SIZE = 32;
 
-struct input_hash_pair {
+class InputHashPair {
+public:
 	const std::string name;
-	const uint8_t *key_data;
-	const size_t key_size;
-	const uint8_t *input_data;
-	const size_t input_size;
+	const std::vector<uint8_t> key;
+	const std::vector<uint8_t> input;
 	const std::array<uint8_t, SHA256_OUTPUT_SIZE> expected_hmac;
 
 	template<size_t N>
-	static uint8_t *copy_input(const std::array<uint8_t, N> &input, size_t &num_bytes) {
-		num_bytes = input.size();
-		auto *raw = new uint8_t[input.size()];
-		std::memcpy(raw, input.data(), input.size());
-		return raw;
+	static std::vector<uint8_t> to_vector(const std::array<uint8_t, N> &input) {
+		std::vector<uint8_t> vector(input.data(), &input.data()[N]);
+		return vector;
 	}
 
 	template<size_t N>
-	static uint8_t *copy_input(const char (&input)[N], size_t &num_bytes) {
-		num_bytes = N - 1;
-		auto *raw = new uint8_t[N - 1];
-		std::memcpy(raw, input, N - 1);
-		return raw;
+	static std::vector<uint8_t> to_vector(const char (&input)[N]) {
+		std::vector<uint8_t> vector(input, &input[N - 1]);
+		return vector;
 	}
 
 	template<typename KeyType, typename InputType>
-	static input_hash_pair create(
-		const std::string &name,
+	InputHashPair(
+		std::string name,
 		const KeyType &key,
 		const InputType &input,
 		const std::array<uint8_t, SHA256_OUTPUT_SIZE> &expected_hmac
-	) {
-		size_t key_size;
-		uint8_t *key_data = copy_input(key, key_size);
-		size_t input_size;
-		uint8_t *input_data = copy_input(input, input_size);
-		return input_hash_pair{name, key_data, key_size, input_data, input_size, expected_hmac};
-	}
+	) :
+		name(std::move(name)),
+		key(to_vector(key)),
+		input(to_vector(input)), expected_hmac(expected_hmac) {}
+
 };
 
-class HmacSha256Test : public testing::TestWithParam<input_hash_pair> {
+class HmacSha256Test : public testing::TestWithParam<InputHashPair> {
 
 };
 
 TEST_P(HmacSha256Test, ComputesHmac) {
 
-	const auto expected_hmac = GetParam().expected_hmac;
-	const auto key_data = GetParam().key_data;
-	const auto key_size = GetParam().key_size;
-	const auto input_data = GetParam().input_data;
-	const auto input_size = GetParam().input_size;
+	const auto &expected_hmac = GetParam().expected_hmac;
+	const auto &key = GetParam().key;
+	const auto &input = GetParam().input;
 
 	std::array<uint8_t, SHA256_OUTPUT_SIZE> hmac{};
 
 	hmac_sha256_ctx_t ctx; // NOLINT(*-pro-type-member-init)
-	hmac_sha256_init(&ctx, key_data, key_size);
-	hmac_sha256_update(&ctx, input_data, input_size);
+	hmac_sha256_init(&ctx, key.data(), key.size());
+	hmac_sha256_update(&ctx, input.data(), input.size());
 	hmac_sha256_final(&ctx, hmac.data());
 
 	EXPECT_SAME_BYTES_S(SHA256_OUTPUT_SIZE, hmac.data(), expected_hmac.data());
@@ -77,13 +70,13 @@ INSTANTIATE_TEST_SUITE_P(
 	HmacSha256Test,
 	testing::Values(
 		// https://en.wikipedia.org/wiki/HMAC#Examples
-		input_hash_pair::create(
+		InputHashPair(
 			"Wiki1",
 			"key",
 			"The quick brown fox jumps over the lazy dog",
 			hex::bytes<"f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8">()
 		),
-		input_hash_pair::create(
+		InputHashPair(
 			"LongKey1",
 			hex::bytes<"da6f806764befd332f5571ad55d3d9957be01ae036fc8161d8a66e87642db49eccb41b2cc473190be39e3178ba901ff5c608228f46ade6ffd654d19302527a7e45798db3e6e23c27ce5661ee657607cf8c17df">(),
 			"The quick brown fox jumps over the lazy dog",
--- test/src/sha256_test.cpp
+++ test/src/sha256_test.cpp
@@ -2,6 +2,8 @@
 #include <gtest_custom_assertions.h>
 #include <hex.hpp>
 #include <cstring>
+#include <utility>
+#include <vector>
 extern "C" {
 #include <sha256.h>
 }
@@ -10,52 +12,48 @@ namespace {
 constexpr size_t SHA256_INTERNAL_BLOCK_SIZE = 64;
 constexpr size_t SHA256_OUTPUT_SIZE = 32;
 
-struct input_hash_pair {
+class InputHashPair {
+
+public:
 	const std::string name;
-	const uint8_t *input_data;
-	const size_t input_size;
+	const std::vector<uint8_t> input;
 	const std::array<uint8_t, SHA256_OUTPUT_SIZE> expected_hash;
 
 	template<size_t N>
-	static input_hash_pair create(
-		const std::string &name,
+	InputHashPair(
+		std::string name,
 		const std::array<uint8_t, N> &input,
 		const std::array<uint8_t, SHA256_OUTPUT_SIZE> &expected_hash
-	) {
-		uint8_t *input_data = new uint8_t[N];
-		// TODO: better solution
-		std::memcpy(input_data, input.data(), N);
-		return input_hash_pair{name, input_data, N, expected_hash};
-	}
+	) :
+		name(std::move(name)),
+		input(input.data(), &input.data()[N]),
+		expected_hash(expected_hash) {}
 
 	template<size_t N>
-	static input_hash_pair create(
-		const std::string &name,
+	InputHashPair(
+		std::string name,
 		const char (&input)[N],
 		const std::array<uint8_t, SHA256_OUTPUT_SIZE> &expected_hash
-	) {
-		uint8_t *input_data = new uint8_t[N - 1];
-		// TODO: better solution
-		std::memcpy(input_data, input, N - 1);
-		return input_hash_pair{name, input_data, N - 1, expected_hash};
-	}
+	) :
+		name(std::move(name)),
+		input(input, &input[N - 1]),
+		expected_hash(expected_hash) {}
 };
 
-class Sha256Test : public testing::TestWithParam<input_hash_pair> {
+class Sha256Test : public testing::TestWithParam<InputHashPair> {
 
 };
 
 TEST_P(Sha256Test, ComputesHash) {
 
-	const auto expected_hash = GetParam().expected_hash;
-	const auto input_data = GetParam().input_data;
-	const auto input_size = GetParam().input_size;
+	const auto &expected_hash = GetParam().expected_hash;
+	const auto &input = GetParam().input;
 
 	std::array<uint8_t, SHA256_OUTPUT_SIZE> hash{};
 
 	SHA256_CTX sha256_ctx; // NOLINT(*-pro-type-member-init)
 	sha256_init(&sha256_ctx);
-	sha256_update(&sha256_ctx, input_data, input_size);
+	sha256_update(&sha256_ctx, input.data(), input.size());
 	sha256_final(&sha256_ctx, hash.data());
 
 	EXPECT_SAME_BYTES_S(SHA256_OUTPUT_SIZE, hash.data(), expected_hash.data());
@@ -66,12 +64,12 @@ INSTANTIATE_TEST_SUITE_P(
 	TestVectors,
 	Sha256Test,
 	testing::Values(
-		input_hash_pair::create(
+		InputHashPair(
 			"ZeroLengthInput",
 			hex::bytes<"">(),
 			hex::bytes<"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855">()
 		),
-		input_hash_pair::create(
+		InputHashPair(
 			"Random1",
 			hex::bytes<
 				"fa8148e2156c99abccbcd15dcaae2122e2afa542260c81716c55b834fcaa5391"
@@ -80,12 +78,12 @@ INSTANTIATE_TEST_SUITE_P(
 			hex::bytes<"e85fede57c8186e9e9e4a156eeac3c4d1255b80ba0becb8c2f05bbd60e1493d6">()
 		),
 		// three test cases from https://github.com/B-Con/crypto-algorithms/blob/master/sha256_test.c
-		input_hash_pair::create(
+		InputHashPair(
 			"GH1",
 			"abc",
 			hex::bytes<"ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad">()
 		),
-		input_hash_pair::create(
+		InputHashPair(
 			"GH2",
 			"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
 			hex::bytes<"248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1">()
