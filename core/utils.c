6dc79c03f11bff0e57e163b22c00ec72204074c9
Martin Endler <pokusew@seznam.cz>
2025-03-27 06:06:18 +0100

    [core] Improve dump_hex

--- core/utils.c
+++ core/utils.c
@@ -3,11 +3,13 @@
 #if LIONKEY_DEBUG_LEVEL > 0
 
 void dump_hex(const uint8_t *buf, size_t size) {
-	printf("hex(%zu): ", size);
+	// TODO: arm-none-eabi does not support %zu?
+	//       see https://stackoverflow.com/questions/76837281/zu-format-specifier-with-c99-not-working
+	debug_log("hex(%u): ", size);
 	while (size--) {
-		printf("%02x", *buf++);
+		debug_log("%02" wPRIx8, *buf++);
 	}
-	printf("\n");
+	debug_log(nl);
 }
 
 #endif
