71da039c161092a1876c13200e2bce2126b6b005
Martin Endler <pokusew@seznam.cz>
2025-04-06 22:42:15 +0200

    [core] Cleanup ctap_pin.h

--- core/ctap_pin.h
+++ core/ctap_pin.h
@@ -2,11 +2,9 @@
 #define LIONKEY_CTAP_PIN_H
 
 #include "ctap.h"
-#include "ctap_parse.h"
 
 uint8_t ctap_client_pin(ctap_state_t *state, const uint8_t *request, size_t length);
 
 void ctap_pin_protocol_v1_init(ctap_pin_protocol_t *protocol);
-
 
 #endif // LIONKEY_CTAP_PIN_H
