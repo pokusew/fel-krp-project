bbaa0d1189f894195de48153bfa8e47b31a663a6
Martin Endler <pokusew@seznam.cz>
2025-05-17 11:51:08 +0200

    [stm32h533] Add missing include guards

--- targets/stm32h533/Inc/app.h
+++ targets/stm32h533/Inc/app.h
@@ -1,3 +1,6 @@
+#ifndef LIONKEY_STM32H33_APP_H
+#define LIONKEY_STM32H33_APP_H
+
 #include <stdnoreturn.h>
 #include "ctaphid.h"
 #include "ctap.h"
@@ -14,3 +17,5 @@ void app_hid_report_send_queue_add(const ctaphid_packet_t *packet);
 void app_hid_report_send_queue_send_one_if_possible(void);
 
 bool app_hid_report_send_queue_is_empty(void);
+
+#endif // LIONKEY_STM32H33_APP_H
--- targets/stm32h533/Inc/usb.h
+++ targets/stm32h533/Inc/usb.h
@@ -1,1 +1,6 @@
+#ifndef LIONKEY_STM32H33_USB_H
+#define LIONKEY_STM32H33_USB_H
+
 void usb_init(void);
+
+#endif // LIONKEY_STM32H33_USB_H
