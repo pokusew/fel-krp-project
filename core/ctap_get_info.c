a834758b7d6054bd458af936457d0ef167ec0857
Martin Endler <pokusew@seznam.cz>
2025-05-17 11:20:43 +0200

    [core] Remove unused ctap_get_info options helpers

--- core/ctap_get_info.c
+++ core/ctap_get_info.c
@@ -1,20 +1,5 @@
 #include "ctap.h"
 
-static const uint32_t ctap_get_info_options_present = 0u
-	| CTAP_getInfo_option_plat
-	| CTAP_getInfo_option_rk
-	| CTAP_getInfo_option_clientPin
-	| CTAP_getInfo_option_up
-	| CTAP_getInfo_option_pinUvAuthToken
-	| CTAP_getInfo_option_credMgmt
-	| CTAP_getInfo_option_makeCredUvNotRqd;
-static const uint32_t ctap_get_info_options_static_values = 0u
-	| CTAP_getInfo_option_rk
-	| CTAP_getInfo_option_up
-	| CTAP_getInfo_option_pinUvAuthToken
-	| CTAP_getInfo_option_credMgmt
-	| CTAP_getInfo_option_makeCredUvNotRqd;
-
 static const COSEAlgorithmIdentifier ctap_supported_pub_key_cred_alg[] = {
 	COSE_ALG_ES256
 };
@@ -33,31 +18,7 @@ bool ctap_is_supported_pub_key_cred_alg(const CTAP_credParams *cred_params) {
 	return false;
 }
 
-bool ctap_get_info_is_option_present(const ctap_state_t *state, const uint32_t option) {
-	return (ctap_get_info_options_present & option) == option;
-}
-
 const uint8_t ctap_aaguid[CTAP_AAGUID_SIZE] = "\xa8\xa1\x47\x32\x6b\x7d\x12\x0d\xfb\x91\x73\x56\xbc\x18\x98\x03";
-
-bool ctap_get_info_is_option_present_with(const ctap_state_t *state, const uint32_t option, const bool value) {
-	if (!ctap_get_info_is_option_present(state, option)) {
-		return false;
-	}
-	// dynamic options
-	if (option == CTAP_getInfo_option_clientPin) {
-		// If present, it indicates that the device is capable of accepting a PIN from the client
-		// ->  if true, it indicates that PIN has been set
-		// -> if false, it indicates that PIN has not been set yet
-		return state->persistent.is_pin_set;
-	}
-	// static options
-	const bool option_value = (ctap_get_info_options_static_values & option) == option;
-	return option_value == value;
-}
-
-bool ctap_get_info_is_option_absent(const ctap_state_t *state, const uint32_t option) {
-	return !ctap_get_info_is_option_present(state, option);
-}
 
 uint8_t ctap_get_info(ctap_state_t *state, CborValue *it, CborEncoder *encoder) {
 
