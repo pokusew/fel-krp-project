4a4afe60e28ccc2381466782020b80da7234e63b
Martin Endler <pokusew@seznam.cz>
2025-05-21 09:01:52 +0200

    [stm32f407] Remove the stm32f407 target and the related obsolete code
    The stm32f407 target (the STM32F407IGH6 MCU on the STM3240G-EVAL board) used the obsolete FIDO2/CTAP2.0 implementation based on the SoloKeys Solo 1 project.
    
    The STM32F407-based FIDO2 Security Key was originally created as my semestral project in the B4M38KRP (Computer Interfaces) course at CTU FEE and later extended as a part of my pre-thesis project (B4MSVP) at CTU FEE. For the LionKey project (my Master's Thesis at CTU FEE), I decided to reimplement everything from scratch and to use the STM32H533 MCU as the main target.
    
    We might bring back the stm32f407 target in the future, when we have time to migrate it to the current LionKey CTAP 2.1 implementation.

--- fido2/time.c
+++
@@ -1,6 +0,0 @@
-#include "time.h"
-
-millis_t timestamp_diff(millis_t start) {
-	millis_t now = millis();
-	return now - start;
-}
--- targets/stm32f407/Inc/rng.h
+++
@@ -1,8 +0,0 @@
-#ifndef POKUSEW_RNG_H
-#define POKUSEW_RNG_H
-
-#include <stdint.h>
-
-void rng_get_bytes(uint8_t *dst, size_t sz);
-
-#endif // POKUSEW_RNG_H
