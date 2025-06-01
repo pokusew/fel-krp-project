71033c649bfc4073a0f27be8dc75f36dc33b7fbd
Martin Endler <pokusew@seznam.cz>
2025-06-02 01:34:00 +0200

    [stm32h533] Improve exception handlers (call Error_Handler())

--- targets/stm32h533/Src/stm32h5xx_it.c
+++ targets/stm32h533/Src/stm32h5xx_it.c
@@ -72,8 +72,7 @@ void NMI_Handler(void) {
 
 	/* USER CODE END NonMaskableInt_IRQn 0 */
 	/* USER CODE BEGIN NonMaskableInt_IRQn 1 */
-	while (1) {
-	}
+	Error_Handler();
 	/* USER CODE END NonMaskableInt_IRQn 1 */
 }
 
@@ -82,12 +81,8 @@ void NMI_Handler(void) {
   */
 void HardFault_Handler(void) {
 	/* USER CODE BEGIN HardFault_IRQn 0 */
-
+	Error_Handler();
 	/* USER CODE END HardFault_IRQn 0 */
-	while (1) {
-		/* USER CODE BEGIN W1_HardFault_IRQn 0 */
-		/* USER CODE END W1_HardFault_IRQn 0 */
-	}
 }
 
 /**
@@ -95,12 +90,8 @@ void HardFault_Handler(void) {
   */
 void MemManage_Handler(void) {
 	/* USER CODE BEGIN MemoryManagement_IRQn 0 */
-
+	Error_Handler();
 	/* USER CODE END MemoryManagement_IRQn 0 */
-	while (1) {
-		/* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
-		/* USER CODE END W1_MemoryManagement_IRQn 0 */
-	}
 }
 
 /**
@@ -108,12 +99,8 @@ void MemManage_Handler(void) {
   */
 void BusFault_Handler(void) {
 	/* USER CODE BEGIN BusFault_IRQn 0 */
-
+	Error_Handler();
 	/* USER CODE END BusFault_IRQn 0 */
-	while (1) {
-		/* USER CODE BEGIN W1_BusFault_IRQn 0 */
-		/* USER CODE END W1_BusFault_IRQn 0 */
-	}
 }
 
 /**
@@ -121,12 +108,8 @@ void BusFault_Handler(void) {
   */
 void UsageFault_Handler(void) {
 	/* USER CODE BEGIN UsageFault_IRQn 0 */
-
+	Error_Handler();
 	/* USER CODE END UsageFault_IRQn 0 */
-	while (1) {
-		/* USER CODE BEGIN W1_UsageFault_IRQn 0 */
-		/* USER CODE END W1_UsageFault_IRQn 0 */
-	}
 }
 
 /**
